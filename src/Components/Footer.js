eaa236d0ef83b291fd086fd63b8374b981b53279
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-08-16 19:10:35 +0500

    works updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
+              <a title="PAPA" href="/">
                 Muhammad Hammad Shahid
               </a>
             </li>
